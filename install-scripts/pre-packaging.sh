705d9619dc45ae4191207f8e3cecda9bd3820ecc
pozil <philippe.ozil@gmail.com>
2020-12-02 10:28:18 +0100

    Moved and renamed pre-packaging script

---
+++ install-scripts/pre-packaging.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd $SCRIPT_PATH/..
+
+PACKAGING_ORG_ALIAS="streaming-prod"
+TEMP_DIR="mdapi"
+
+echo "Adding namespace in DX project..." && \
+sed -i '' -e 's,"namespace": "","namespace": "smon",' sfdx-project.json && \
+echo "" && \
+
+echo "Deploying to packaging org..." && \
+sfdx force:source:deploy -p src -u $PACKAGING_ORG_ALIAS && \
+echo "" && \
+
+echo "Restoring project config..." && \
+git checkout -- sfdx-project.json
+EXIT_CODE="$?"
+
+# Check exit code
+echo ""
+if [ "$EXIT_CODE" -eq 0 ]; then
+    echo "Deployment completed."
+else
+    echo "Deployment failed."
+fi
+exit $EXIT_CODE

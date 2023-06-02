89f2c0d2c10cd583ac455820c5022ac0421bb212
Philippe Ozil <pozil@salesforce.com>
2023-06-02 10:20:36 +0200

    build: migrate to sf CLI (#37)
    * build: migrated to sf CLI
    
    * ci: switch to node CLI install
    
    * ci: remove JSON output for create scratch org
    
    * ci: use npm global location flag for CLI install

--- install-dev.bat
+++ install-dev.bat
@@ -8,21 +8,21 @@ echo Installing Streaming Monitor DEV org (%ORG_ALIAS%)
 
 rem Install script
 echo Cleaning previous scratch org...
-cmd.exe /c sfdx force:org:delete -p -u %ORG_ALIAS% 2>NUL
+cmd.exe /c sf org delete scratch -p -o %ORG_ALIAS% 2>NUL
 @echo:
 
 echo Creating scratch org...
-cmd.exe /c sfdx force:org:create -s -f config/project-scratch-def.json -a %ORG_ALIAS% -d 30
+cmd.exe /c sf org create scratch -f config/project-scratch-def.json -a %ORG_ALIAS% -d -y 30
 call :checkForError
 @echo:
 
 echo Pushing source...
-cmd.exe /c sfdx force:source:push -f -u %ORG_ALIAS%
+cmd.exe /c sf project deploy start
 call :checkForError
 @echo:
 
 echo Assigning permissions...
-cmd.exe /c sfdx force:user:permset:assign -n Streaming_Monitor -u %ORG_ALIAS%
+cmd.exe /c sf org assign permset -n Streaming_Monitor
 call :checkForError
 @echo:
 
@@ -31,7 +31,7 @@ rem Check exit code
 if ["%errorlevel%"]==["0"] (
   echo Installation completed.
   @echo:
-  cmd.exe /c sfdx force:org:open -p lightning/n/Streaming_Monitor -u %ORG_ALIAS%
+  cmd.exe /c sf org open -p lightning/n/Streaming_Monitor
 )
 
 :: ======== FN ======
